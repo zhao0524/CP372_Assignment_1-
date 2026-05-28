8d3f4eed209898a282fc6f49a3f2502306b63b12
Hans Yang <hans.yangchenghao@gmail.com>
2026-05-28 18:08:45 -0400

    template

--- client.py
+++ client.py
@@ -1,4 +1,24 @@
 import socket
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# print("Socket created")
+HOST = '127.0.0.1'   # Server IP
+PORT = 5000          # Server port
+
+# Create socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to server
+client_socket.connect((HOST, PORT))
+
+while True:
+    message = input("You: ")
+
+    client_socket.sendall(message.encode())
+
+    data = client_socket.recv(1024)
+
+    print("Server:", data.decode())
+
+    if message.lower() == "exit":
+        break
+
+client_socket.close()
--- server.py
+++ server.py
@@ -1,4 +1,34 @@
 import socket
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# print("Socket created")
+HOST = '127.0.0.1'   # Localhost
+PORT = 5000          # Port to listen on
+
+# Create socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind socket to address and port
+server_socket.bind((HOST, PORT))
+
+# Start listening
+server_socket.listen(1)
+
+print(f"Server listening on {HOST}:{PORT}")
+
+# Accept client connection
+conn, addr = server_socket.accept()
+print(f"Connected by {addr}")
+
+while True:
+    data = conn.recv(1024)
+
+    if not data:
+        break
+
+    message = data.decode()
+    print("Client:", message)
+
+    reply = input("Server reply: ")
+    conn.sendall(reply.encode())
+
+conn.close()
+server_socket.close()
